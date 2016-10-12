5edfc2d5f7b5de74aae38979fb39d7bfa4891ae5
daysip5272 <daysip5272@hstat.org>
2016-10-12 23:32:07 +0000

    finish hw

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,24 @@
-def division
-    
+def division(number_1,number_2)
+    number_1/number_2
 end
 
-def argue
-  
+def argue(fight)
+  fight
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
-    
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(yum="cheese")
+    return yum
 end
 
-def whisper
+def whisper(sshh)
+    return sshh.downcase
     
 end
